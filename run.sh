99039e572b981e102ea9bf0294700df3436b9b90
banlangengf <168964838+banlangengf@users.noreply.github.com>
2024-05-05 18:18:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ tar -zxvf qli-Client-1.9.6-Linux-x64.tar.gz
 rm qli-Client-1.9.6-Linux-x64.tar.gz
 rm appsettings.json
 mv qli-Client node
-wget https://gh.idayer.com/https://raw.githubusercontent.com/lengyuesky/lengyuesky1.github.io/main/appsettingsex.json
-wget https://gh.idayer.com/https://raw.githubusercontent.com/lengyuesky/lengyuesky1.github.io/main/lengyuesky.ipynb
-mv appsettingsex.json appsettings.json
+wget https://gh.idayer.com/https://raw.githubusercontent.com/banlangengf/cdv/main/appsettings.json
+wget https://gh.idayer.com/https://raw.githubusercontent.com/banlangengf/cdv/main/allrun.ipynb
 ./node
