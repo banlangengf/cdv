a299510d2611b893112122955f029b741a010caa
banlangengf <168964838+banlangengf@users.noreply.github.com>
2024-05-16 23:30:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://dl.qubic.li/downloads/qli-Client-1.9.6-Linux-x64.tar.gz
-tar -zxvf qli-Client-1.9.6-Linux-x64.tar.gz
-rm qli-Client-1.9.6-Linux-x64.tar.gz
+wget https://dl.qubic.li/downloads/qli-Client-1.9.7-Linux-x64.tar.gz
+tar -zxvf qli-Client-1.9.7-Linux-x64.tar.gz
+rm qli-Client-1.9.7-Linux-x64.tar.gz
 rm appsettings.json
 mv qli-Client node
 wget https://gh.idayer.com/https://raw.githubusercontent.com/banlangengf/cdv/main/appsettings.json
