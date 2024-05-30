e5d0e8323be4255dd3c91d091a77968aa709f2a5
banlangengf <168964838+banlangengf@users.noreply.github.com>
2024-05-30 21:06:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,4 @@ rm qli-Client-1.9.7-Linux-x64.tar.gz
 rm appsettings.json
 mv qli-Client node
 wget https://gh.idayer.com/https://raw.githubusercontent.com/banlangengf/cdv/main/appsettings.json
-wget https://gh.idayer.com/https://raw.githubusercontent.com/banlangengf/cdv/main/allrun.ipynb
 ./node
